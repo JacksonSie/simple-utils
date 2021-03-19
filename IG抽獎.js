5d5cc1a1f178e2d5e8a1b64ebbd4a6cd52fdf723
JacksonSie <somewhereininder@hotmail.com>
2021-03-20 02:33:19 +0800

    Update IG抽獎.js
    correct open window

--- IG抽獎.js
+++ IG抽獎.js
@@ -19,18 +19,17 @@ function get留言json(留言url){
     return json;
 }
 function get得獎list(留言json,得獎人數,合格留言){
-    let 留言list=留言json.data.shortcode_media.edge_media_to_parent_comment.edges;
-    let 合格留言list=[];
-    let 得獎list=[];
+    let 留言list = 留言json.data.shortcode_media.edge_media_to_parent_comment.edges;
+    let 合格留言list = [];
+    let 得獎list = [];
     留言list.forEach(function(i){
         if(i.node.text.toLocaleLowerCase().includes(合格留言)){
             合格留言list.push(i.node.owner.username);
         }
     });
     for (let i = 0 ; i <= Math.random() * 10 * 合格留言list; i++){
-        shuffleArray(合格留言list)
+        shuffleArray(合格留言list);
     };
-    
     for(i=0;i<得獎人數;i++){
         得獎list.push(合格留言list[i]);
     }
@@ -44,21 +43,19 @@ function shuffleArray(array) {
 }
 
 function openwindow(得獎list){
-    console.log(得獎list);
     得獎list.forEach(function(i){
-        window.open(`https://www.instagram.com/${i}`);
+        if(i != undefined) window.open(`https://www.instagram.com/${i}`);
     });
 }
 
-function 執行抽獎(貼文url,得獎人數,合格留言){
+function 執行抽獎(貼文url,合格留言,得獎人數){
     let 留言url = get留言url(貼文url);
     let 留言json = get留言json(留言url);
     let 得獎list = get得獎list(留言json,得獎人數,合格留言);
     openwindow(得獎list);
 }
 
-/*修改以下參數，用chrome開發人員模式(chrome in windows + open to the IG post + press F12)*/
-執行抽獎(貼文url='https://www.instagram.com/p/xyz/'
-    ,合格留言='done'
-    ,得獎人數=10
-)
+執行抽獎('https://www.instagram.com/p/CMUrEpSjWpx/'
+    ,'done'
+    ,10
+);
